f310b7c81d4e8905b75a49b9af13581c5f0cae04
Antoninochin <antonino_chindemi@virgilio.it>
2022-09-30 18:14:20 +0200

    js

--- Progetto===true/js/script.js
+++ Progetto===true/js/script.js
@@ -1,9 +1,12 @@
-
-function card(event){
+let cardOne;
+let cardTwo
+ function card(event){
     let itemSelected = event.target
-    itemSelected.classList.add('display')
-    itemSelected.ClassList.remove('flexItem')
-    
-}
+   itemSelected.classList.add('display')
+     itemSelected.ClassList.remove('flexItem')
+ }
 
+// function card(event){
+//     let cardOne = document.querySelectorAll('')
+// }
 
